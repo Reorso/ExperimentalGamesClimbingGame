37ea35166f0eeb5a26108452585c3c22c4ba1be0
Reorso <reorso@hotmail.it>
2021-11-04 15:56:50 +0000

    Initial Commit

---
+++ Assets/Scripts/Limb.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class Limb : MonoBehaviour
+{
+    float lenght;
+    public Transform target;
+    public ChainIKConstraint chik;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+        }
+    }
+}

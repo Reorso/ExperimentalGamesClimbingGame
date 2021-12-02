ff9c8e2efc9b9527ee997c9aa8f35c9da5633a8d
Reorso <reorso@hotmail.it>
2021-12-02 00:50:38 +0000

    added fkagging

--- Assets/Scripts/ClimbController.cs
+++ Assets/Scripts/ClimbController.cs
@@ -11,6 +11,7 @@ public class ClimbController : MonoBehaviour
     private Transform centre;
     public TwoBoneIKConstraint[] limbs;
     private float[] lenghts;
+    private bool[] locked;
     public BlendConstraint centreC;
     private bool hipsMoving = false;
     Vector3[] pointsAttached;
@@ -25,6 +26,7 @@ public class ClimbController : MonoBehaviour
         lenghts = new float[4];
         centre = centreC.data.sourceObjectA;
         pointsAttached = new Vector3[4];
+        locked = new bool[4];
         ResetLimbs();
         CalculateLenghts();
     }
@@ -38,40 +40,39 @@ public class ClimbController : MonoBehaviour
                 item.GetComponent<MeshRenderer>().enabled = !item.GetComponent<MeshRenderer>().enabled;
             }
         }
-    }
-
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
         if (Input.GetKeyDown(KeyCode.B))
         {
             SceneManager.LoadScene(0);
         }
-        if (Input.GetMouseButtonDown(0))
+
+
+        if (Input.GetMouseButton(0))
         {
             var ray = cam.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out var hit, 100, LayerMask.GetMask("Route")))
+            if (Physics.Raycast(ray, out var hit, 100, LayerMask.GetMask("Wall")))
             {
-                if(centre.position.y < hit.point.y){ 
-                    MoveLimb(0, hit.point);
-                }
-                else{
-                    MoveLimb(2, hit.point);
-                }
-            }
-        }
-        else if (Input.GetMouseButtonDown(1))
-        {
-            var ray = cam.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out var hit, 100 , LayerMask.GetMask("Route")))
-            {
-                if(centre.position.y < hit.point.y){
-                    MoveLimb(1, hit.point);
+                if (centre.position.y < hit.point.y)
+                {
+                    MoveLimb(0, hit.point, false);
                 }
                 else
                 {
-                    MoveLimb(3, hit.point);
+                    MoveLimb(2, hit.point, false);
+                }
+            }
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            var ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit, 100, LayerMask.GetMask("Wall")))
+            {
+                if (centre.position.y < hit.point.y)
+                {
+                    MoveLimb(1, hit.point, false);
+                }
+                else
+                {
+                    MoveLimb(3, hit.point, false);
                 }
             }
         }
@@ -95,6 +96,40 @@ public class ClimbController : MonoBehaviour
             ResetLimbs();
         }
 
+        if (Input.GetMouseButtonUp(0)) 
+        {
+            var ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit, 100, LayerMask.GetMask("Route")))
+            {
+                if (centre.position.y < hit.point.y)
+                {
+                    MoveLimb(0, hit.collider.transform.position,true);
+                }
+                else
+                {
+                    MoveLimb(2, hit.collider.transform.position, true);
+                }
+            }
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            var ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit, 100, LayerMask.GetMask("Route")))
+            {
+                if (centre.position.y < hit.point.y)
+                {
+                    MoveLimb(1, hit.collider.transform.position, true);
+                }
+                else
+                {
+                    MoveLimb(3, hit.collider.transform.position, true);
+                }
+            }
+        }
+
+
+
+
         if (Input.GetMouseButtonUp(2) && hipsMoving)
         {
             //centreC.weight = 0;
@@ -102,13 +137,25 @@ public class ClimbController : MonoBehaviour
         }
     }
 
-    private void MoveLimb(int n, Vector3 point)
+
+
+    private void MoveLimb(int n, Vector3 point, bool islocked)
     {
+        Vector3 newpos = new Vector3();
         limbs[n].weight = 1;
-        Vector3 newpos = limbs[n].data.root.position + ((point - limbs[n].data.root.position ).normalized *  Mathf.Clamp((point - limbs[n].data.root.position ).magnitude,0,lenghts[n]));
+        
+        if (islocked)
+        {
+            limbCounter++;
+            locked[n] = true;
+            newpos = point;
+        }
+        else
+        {
+            newpos = limbs[n].data.root.position + ((point - limbs[n].data.root.position).normalized * Mathf.Clamp((point - limbs[n].data.root.position).magnitude, 0, lenghts[n]));
+        }
+        pointsAttached[n] = newpos;
         limbs[n].data.target.position = newpos;
-        pointsAttached[n] = point;
-        limbCounter++;
         centroid.transform.position = limbCounter >= 3 ? FindCentroid(pointsAttached) : transform.position;
     }
     private void ResetLimbs()
